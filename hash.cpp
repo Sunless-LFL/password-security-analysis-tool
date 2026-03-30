248cf77024e05ca08faf4dda49900f6b64d3b7d8
Sunless-LFL <benchagraiyad1@gmail.com>
2026-03-31 00:58:58 +0100

    Add hash.cpp implementation for hashing module (Phase 2)

---
+++ hash.cpp
@@ -0,0 +1,22 @@
+#include "hash.h"
+#include <openssl/evp.h>
+#include <sstream>
+#include <iomanip>
+
+std::string sha256(const std::string& input) {
+    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, EVP_sha256(), nullptr);
+    EVP_DigestUpdate(ctx, input.c_str(), input.size());
+
+    unsigned char hash[32];
+    unsigned int  len = 0;
+    EVP_DigestFinal_ex(ctx, hash, &len);
+    EVP_MD_CTX_free(ctx);
+
+    std::ostringstream oss;
+    for (unsigned int i = 0; i < len; i++) {
+        oss << std::hex << std::setw(2) << std::setfill('0')
+            << (int)hash[i];
+    }
+    return oss.str();
+}
